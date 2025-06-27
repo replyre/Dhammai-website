33a03ffe788f3de0b2b33599a8d9bc7d97716834
Shivam Kumar Saraffa <ssaraffa786@gmail.com>
2025-06-27 17:42:04 +0530

    almost

--- app/technology/t1/components/HeroSection.jsx
+++ app/technology/t1/components/HeroSection.jsx
@@ -6,11 +6,13 @@ const HeroSection = () => {
   return (
     <div className={`${styles.contentContainer} ${styles.section}`}>
       {/* Back Arrow */}
-      <button className={styles.backButton} >
-        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M19 12H5M12 19L5 12L12 5" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-        </svg>
-      </button>
+      <div className={styles.backButtonContainer}>
+        <button className={styles.backButton} >
+          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M19 12H5M12 19L5 12L12 5" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+          </svg>
+        </button>
+      </div>
 
       {/* Main Content */}
       <div className={styles.content}>
